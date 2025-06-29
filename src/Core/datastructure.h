57ddf13d7404c9220f532a3b03e8bf3719e2c37d
Niladri Das <niladridas702@gmail.com>
2025-06-29 16:41:19 +0530

    Custom lib & dynamic memory_datatype complete

---
+++ src/Core/datastructure.h
@@ -0,0 +1,18 @@
+#ifndef DATASTRUCTURE_H
+#define DATASTRUCTURE_H
+#include<Lib/Lib.h>
+#include<Memory/dynamic.h>
+
+struct Array{
+    void *Castintoanothertype;
+    int Maxsize =INT_MAX;
+    int Maxsize =INT_MIN;
+    //Cout the current stage
+    int counter;
+
+};
+
+
+
+
+#endif
---
+++ src/Lib/Lib.h
@@ -0,0 +1,24 @@
+#ifndef LIB_H
+#define LIB_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdarg.h>
+#include<string.h>
+#include<stdbool.h>
+#include<math.h>
+#include<limits.h>
+#include<threads.h>
+#include<assert.h>
+#include<float.h>
+#include<complex.h>
+#include<time.h>
+#include<wctype.h>
+#include<stdalign.h>
+#include<errno.h>
+#include<fenv.h>
+
+
+
+
+#endif
